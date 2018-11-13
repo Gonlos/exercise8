00397ed6bfd398e26813bd15c519f7a4b1c18624
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-11-13 01:41:40 +0100

    Config stats on haproxy, change endpoint /hostname for /heaath and config stats to use /health

--- message/index.js
+++ message/index.js
@@ -7,7 +7,7 @@ const { Validator, ValidationError } = require("express-json-validator-middlewar
 const sendMessage = require("./src/controllers/EnqueueSendMessage");
 const getMessages = require("./src/controllers/getMessages");
 const getMessageStatus = require("./src/controllers/getMessageStatus");
-const hostname = require("./src/controllers/hostname");
+const health = require("./src/controllers/health");
 require("./src/queues/dispatcherMessage");
 require("./src/clients/futureCredit");
 
@@ -37,7 +37,7 @@ const messageSchema = {
   }
 };
 
-app.get("/hostname", hostname);
+app.get("/health", health);
 app.post("/messages", bodyParser.json(), validate({ body: messageSchema }), sendMessage);
 
 app.get("/messages", getMessages);
